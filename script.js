4118734187f5876f20a5baa6c93ec5b219cc8767
rupeshk540 <162593730+rupeshk540@users.noreply.github.com>
2024-04-21 01:22:25 +0530

    Score Updation
    Adds a function to show the user score and computer score.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 const genCompChoice =()=>{
     const options = ["rock","paper","scissors"];
     const randIdx = Math.floor(Math.random()*3);
@@ -17,9 +20,13 @@ const drawGame=()=>{
 
 const showWinner = (userWin,userChoice,compChoice)=>{
     if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
        msg.style.backgroundColor ="green";
     }else{
+        compScore++;
+        compScorePara.innerText = compScore;
        msg.innerText = `You lose! ${compChoice} beats Your ${userChoice}`;
        msg.style.backgroundColor ="red";
     }
